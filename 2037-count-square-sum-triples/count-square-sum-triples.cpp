e7b0547ccd19e066dc34e2e2b1f328cce116042e
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2025-12-08 11:32:48 +0530

    Time: 3 ms (90.49%) | Memory: 7.8 MB (84.51%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int sqrt(int n){
-        int lo = 0,hi = n;
-        while(lo <= hi){
-            long long mid = (lo + hi) / 2;
-            long long sq = mid * mid;
-            if(sq == n) return mid;
-            if(sq > n) hi = mid-1;
-            else lo = mid+1;
-        }
-        return -1;
-    }
     int countTriples(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                int c = (i*i) + (j*j);
-                c = sqrt(c); 
-                if(c!= -1 && c <= n)ans++;
+        int count = 0;
+        
+        for (int c = 1; c <= n; ++c) {
+            int target = c * c;
+            int left = 1;
+            int right = c - 1;
+            
+            while (left < right) {
+                int sum = left * left + right * right;
+                
+                if (sum == target) {
+                    count += 2;
+                    left++;
+                    right--;
+                } 
+                else if (sum < target) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
             }
         }
-        return ans;
+        
+        return count;
     }
 };
