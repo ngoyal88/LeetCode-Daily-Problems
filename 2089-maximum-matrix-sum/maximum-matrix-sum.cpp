39a43c800b19d0001be600b75c449d9f95aa5186
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2026-01-05 17:42:40 +0530

    Time: 3 ms (46.23%) | Memory: 39 MB (88.21%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -4,8 +4,8 @@ public:
         long long sum = 0;
         int cnt = 0;
         int mini = INT_MAX;
-        for (auto row : matrix) {
-            for (int it : row) {
+        for (auto &row : matrix) {
+            for (int &it : row) {
                 if (it < 0) cnt++;
                 it = abs(it);
                 sum += it;
