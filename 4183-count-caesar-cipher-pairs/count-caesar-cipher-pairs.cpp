e2706c2d60d5f612b512cb32eecebfb7fbc46d9f
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2026-01-13 01:28:21 +0530

    Time: 37 ms (64.01%) | Memory: 110.4 MB (78.10%) - LeetSync

--- 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.cpp
+++ 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.cpp
@@ -6,11 +6,11 @@ public:
         long long ans=0;
         unordered_map<string,int> mp;
         for(int i=0;i<n;i++){
-            string key = "";
+            string key;
+            key.reserve(m - 1);
             for(int j=0;j<m-1;j++){  
                 int diff = (words[i][j+1] - words[i][j] + 26)%26;
-                if(diff > 9) key += to_string(diff);
-                else key += "0" + to_string(diff);
+                key.push_back(char(diff));
             }
             if(mp.count(key))ans+= mp[key];
             mp[key]++;
